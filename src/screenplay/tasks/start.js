bba6cb78e61ba3745e3e98bc14e27ec6eb3b5fce
InvictusMB <invictusmb@gmail.com>
2017-02-04 11:45:47 +0100

    Rewrite Start task with task builder

--- src/screenplay/tasks/start.js
+++ src/screenplay/tasks/start.js
@@ -1,26 +1,20 @@
-import {decorate} from '../../decorate';
-import {Open, step} from 'serenity-js/lib/screenplay-protractor';
+import {Open} from 'serenity-js/lib/screenplay-protractor';
 
 import {AddATodoItem} from './add_a_todo_item.js';
+import {defineTask} from '../../taskBuilder';
 
-export function Start(items) {
-  this.items = items;
-}
+const addAllTodos = ({items}, actor) => {
+  const addItems = items.map(item => AddATodoItem.called(item));
+  return actor.attemptsTo(
+    ...addItems
+  );
+};
 
-Start.prototype = {
-  performAs(actor) {
-    return actor.attemptsTo(
+export const Start =
+  defineTask()
+    .describe('{0} starts with a Todo List containing #items')
+    .addActions(
       Open.browserOn('/examples/angularjs/'),
-      ...this.addAll(this.items)
-    );
-  },
-  addAll(items) {
-    return items.map(item => AddATodoItem.called(item));
-  }
-};
-
-decorate(Start.prototype, "performAs", step('{0} starts with a Todo List containing #items'));
-
-Start.withATodoListContaining = function(items) {
-  return new Start(items);
-};
+      addAllTodos
+    )
+    .defineProps({withATodoListContaining: 'items'});
